3bfa6b428f9e991d61aaf25c052d1392ee90d8ee
bene-labs <62158116+bene-labs@users.noreply.github.com>
2025-02-01 21:32:22 +0100

    add new music, adjust asteroids, add hard mode, fix diffculty scaling

--- scripts/obstacle_spawner.gd
+++ scripts/obstacle_spawner.gd
@@ -1,17 +1,24 @@
 extends Node2D
 
-const INIT_MIN_OBSTACLE := 30
-const INIT_MAX_OBSTACLE := 60
+signal intense_mode_started
+
+const INIT_MIN_OBSTACLE := 1
+const INIT_MAX_OBSTACLE := 5
 
 var min_obstacles := INIT_MIN_OBSTACLE
 var max_obstacles := INIT_MAX_OBSTACLE
 var diffculty = 1.0
+var started_intense_mode := false
 
 
 func _process(delta: float) -> void:
-	diffculty = 1.0 + Player.traveled_distance / 100000
-	min_obstacles = int(INIT_MIN_OBSTACLE + diffculty * 1.5)
-	max_obstacles = int(INIT_MAX_OBSTACLE + diffculty * 2)
+	diffculty = 1.0 + Player.traveled_distance / 10_000
+	if not started_intense_mode and diffculty >= 3:
+		AudioController.play_intense()
+		intense_mode_started.emit()
+		started_intense_mode = true
+	min_obstacles = int(INIT_MIN_OBSTACLE + diffculty / 4)
+	max_obstacles = int(INIT_MAX_OBSTACLE + diffculty / 2)
  
 
 func spawn_asteroid():
@@ -30,9 +37,11 @@ func spawn_cat_food():
 	var col_shape = %SpawnArea.get_child(0)
 	var centerpos = col_shape.global_position
 	var size = col_shape.shape.extents
+	var min = centerpos - size
+	var max = centerpos + size
 	var position_in_area : Vector2
-	position_in_area.x = (randi() % int(size.x)) - (size.x/2) + centerpos.x
-	position_in_area.y = (randi() % int(size.y)) - (size.y/2) + centerpos.y
+	position_in_area.x = randf_range(min.x, max.x)
+	position_in_area.y = randf_range(min.y, max.y)
 	await get_tree().process_frame
 	$"../Obstacles".add_child(CatFood.spawn(diffculty, position_in_area))
 
@@ -41,6 +50,6 @@ func _on_new_area_trigger_body_entered(body):
 	print(body.name)
 	for i in range(randi_range(min_obstacles, max_obstacles)):
 		spawn_asteroid()
-	for i in range(randi_range(1, 2)):
+	for i in range(int(randf_range(1, 2 + diffculty / 2))):
 		spawn_cat_food()
 	global_position = Vector2(global_position.x + %DimensionRef.size.x, global_position.y)
